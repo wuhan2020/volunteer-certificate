24028c4c0662e131dfabd9f21e915440f28ad80d
zhaofeng-shu33 <616545598@qq.com>
2020-03-02 01:05:51 +0800

    add generate pic.jpg on the fly

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
+import os
 from unittest.mock import patch
 import unittest
+
 import yagmail
+from PIL import Image
+
 from pic_email import write_to_pic
 
 class SubmitUserInfoTests(unittest.TestCase):
-    with patch("yagmail.SMTP") as mock_smtp:
-        write_to_pic('test name', 'muxxs@foxmail.com')
+    def test_generate_image_and_send_email(self):
+        if not os.path.exists('pic.jpg'):
+            img = Image.new('RGB', (750, 1200))
+            img.save('pic.jpg')
+        with patch("yagmail.SMTP") as mock_smtp:
+            write_to_pic('test name', 'muxxs@foxmail.com')
