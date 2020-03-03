e0eacf26492b6ec872b8129f9057e78917361f25
Xin Wang <xin.victorwang@gmail.com>
2020-03-03 22:11:38 +0800

    add link in email content (#50)
    * add link in email content
    
    * Update jobs.py
    
    Co-authored-by: 赵丰 (Zhao Feng) <616545598@qq.com>

--- jobs.py
+++ jobs.py
@@ -19,8 +19,9 @@ def send_notice_email():
     for user in target_users:
         send_email(
             to_email=user['email'],
-            subject='快来领取您的wuhan2020证书',
-            content='感谢您的辛苦付出，请点击链接领取您的志愿证书\n',
+            subject='快来领取您的wuhan2020开源社区志愿者证书',
+            content='感谢您的辛苦付出，请点击链接 https://community.wuhan2020.org.cn/volunteer/update-name.html?token='
+                   + user['token'] + '领取您的《志愿者证书》(一个月内有效)\n',
         )
         update_status(email=user['email'], status=1)
         time.sleep(random.random() * 10)
