f8c3d0f614762b913b8422081a74955e884e9282
YouqingXiaozhua <843213558@qq.com>
2020-03-03 16:01:46 +0800

    1. remove duplicate define of send_email function 2. finely closing file to avoid warning

--- utils.py
+++ utils.py
@@ -4,9 +4,16 @@ import yagmail
 
 
 def send_email(to_email, subject, content, attachment=None):
-    EMAIL_INFO = json.loads(open('email.json').read())
+    with open('email.json') as f:
+        email_json = json.loads(f.read())
     # 链接邮箱服务器
-    yag = yagmail.SMTP(user=EMAIL_INFO['username'], password=EMAIL_INFO['password'], host=EMAIL_INFO['server_address'])
+    yag = yagmail.SMTP(user=email_json["username"],
+                       password=email_json["password"],
+                       host=email_json["server_address"],
+                       port=email_json["smtp_port"],
+                       smtp_ssl=email_json["smtp_ssl"],
+                       smtp_starttls=email_json["smtp_ssl"],
+                       smtp_skip_login=email_json["skip_auth"])
     # 邮箱正文
     # contents = ['您好，附件中有您的证书']
     print("send to " + to_email, 'with subject', subject)
