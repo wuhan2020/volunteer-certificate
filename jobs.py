89b8febd73c7df120f953f54bb7adf9b25b0e59a
赵丰 (Zhao Feng) <616545598@qq.com>
2020-03-03 21:59:11 +0800

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -20,7 +20,8 @@ def send_notice_email():
         send_email(
             to_email=user['email'],
             subject='快来领取您的wuhan2020开源社区志愿者证书',
-            content='感谢您的辛苦付出，请点击链接https://community.wuhan2020.org.cn/volunteer/update-name.html?token='+user['token']+'领取您的《志愿者证书》(一个月内有效)\n',
+            content='感谢您的辛苦付出，请点击链接 https://community.wuhan2020.org.cn/volunteer/update-name.html?token='
+                   + user['token'] + '领取您的《志愿者证书》(一个月内有效)\n',
         )
         update_status(email=user['email'], status=1)
         time.sleep(random.random() * 10)
