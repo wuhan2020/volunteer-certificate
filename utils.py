261444aab010016b01eb1682c8dd76cd9ee280e9
赵丰 (Zhao Feng) <616545598@qq.com>
2020-03-04 12:51:59 +0800

    Revert "删除多余的邮箱配置项" (#56)
    * Revert "删除多余的邮箱配置项 (#53)"
    
    This reverts commit dda7bb686cfd0fa7b9faadb13f750500653a48d6.
    
    * Update utils.py
    
    Co-authored-by: Xin Wang <xin.victorwang@gmail.com>

--- utils.py
+++ utils.py
@@ -16,9 +16,11 @@ def send_email(to_email, subject, content, attachment=None):
                        host=email_json["server_address"],
                        port=email_json["smtp_port"],
                        smtp_ssl=email_json["smtp_ssl"],
-                       )
+                       smtp_starttls=email_json["smtp_ssl"] and email_json["skip_auth"],
+                       smtp_skip_login=email_json["skip_auth"])
     # 邮箱正文
     # contents = ['您好，附件中有您的证书']
     print("send to " + to_email, 'with subject', subject)
     # 发送邮件
     yag.send(to_email, subject, content, attachment)
+
