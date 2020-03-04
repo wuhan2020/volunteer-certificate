c77b8cf6f5bebfe95beddfbf30fa9ce20e74a714
Xin Wang <xin.victorwang@gmail.com>
2020-03-04 23:09:43 +0800

    add start script

---
+++ bin/stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+name=$(lsof -i:5000|tail -1|awk '"$1"!=""{print $2}')
+if [ -z $name ]
+then
+    echo "No process can be used to killed!"
+    exit 0
+fi
+id=$(lsof -i:80|tail -1|awk '"$1"!=""{print $2}')
+kill -9 $id
+
+echo "Process name=$name($id) kill!"
+exit 0
