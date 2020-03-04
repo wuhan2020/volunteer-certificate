1f5a5739f41e4b9136e3b9daeeb151eb09c5ff82
Xin Wang <xin.victorwang@gmail.com>
2020-03-04 21:57:48 +0800

    Update pic_email.py

--- pic_email.py
+++ pic_email.py
@@ -10,7 +10,7 @@ from utils import send_email
 def write_to_pic(name, email):  # 执行完这个方法后生成一个 result.png 图片 可加入email参数
     number = get_number(email)  # 用这个方法获取到编号
     update_status(email)  # use 参数 变为1  生成了证书
-    im = Image.open("pic.jpeg")
+    im = Image.open("pic.jpg")
     draw = ImageDraw.Draw(im)
     font_name = ImageFont.truetype('font/1.ttf', 55)  # 名字的字体和字号
     font_number = ImageFont.truetype('font/1.ttf', size=35)  # 编号的字体和字号
