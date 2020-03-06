c95f510379b88c8d647c1412940520f8fb93fb11
Egria <ned1_32@sina.com>
2020-03-06 14:30:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ class SubmitUserInfoTests(unittest.TestCase):
             img = Image.new('RGB', (750, 1200))
             img.save('pic.jpg')
         with patch("yagmail.SMTP") as mock_smtp:
-            write_to_pic('test name', 'muxxs@foxmail.com')
+            write_to_pic('test name', 'muxxs@foxmail.com', 'idle_token')
 
     def test_notice_email(self):
         with patch("yagmail.SMTP"):
