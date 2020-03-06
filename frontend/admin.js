7843efabc3918ac195a049845fc8253fe5e4de31
zhaofeng-shu33 <616545598@qq.com>
2020-03-06 21:59:12 +0800

    update admin.js

--- frontend/admin.js
+++ frontend/admin.js
@@ -11,11 +11,23 @@ export default class Admin extends React.Component {
     return (
     <div>
       Admin page
+      <form>
       <div class="input-group mb-3">
       <div class="input-group-prepend"><span class="input-group-text">
         Token</span></div>
       <input type="password" id="token" className="form-control" />
       </div>
+      <div class="form-group">
+        <label for="template">Template Image</label>
+        <input type="file" class="form-control-file"
+               id="template" accept=".jpg"/>
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      </form>
     </div>
     )
   }
