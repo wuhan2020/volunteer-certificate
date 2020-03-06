27e4b7895dd7e7d123b6ffdb14af21241abb8459
zhaofeng-shu33 <616545598@qq.com>
2020-03-06 21:43:47 +0800

    add token input

--- frontend/admin.js
+++ frontend/admin.js
@@ -11,6 +11,11 @@ export default class Admin extends React.Component {
     return (
     <div>
       Admin page
+      <div class="input-group mb-3">
+      <div class="input-group-prepend"><span class="input-group-text">
+        Token</span></div>
+      <input type="password" id="token" className="form-control" />
+      </div>
     </div>
     )
   }
