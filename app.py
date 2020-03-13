19dfbff340f6e80fbd2cac4b2e1792ce201330ea
zhaofeng-shu33 <616545598@qq.com>
2020-03-13 10:00:25 +0800

    add lint config file

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def return_msg(message):
     return message
 
 def check_email(email):
-    regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+    regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
     if re.search(regex, email):
         return True
     return False
