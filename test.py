75d1e23d4ed0437e33167e76ba2ed2478d0672e4
YouqingXiaozhua <843213558@qq.com>
2020-03-16 15:13:27 +0800

    - 完善了单元测试，token0全流程 - 修复了log error - 错误信息记录成error便于查看

--- test.py
+++ test.py
@@ -1,71 +1,73 @@
-import os
-from unittest.mock import patch
+import json
 import unittest
 from shutil import copyfile
+from unittest.mock import patch
 
-import json
-import yagmail
-from PIL import Image
-
-from jobs import SendEmailJob
-from model import insert_people
-from pic_email import write_to_pic
 from app import app
-from utils import get_smtp_url
+from model import insert_people
 from utils import get_email_config
 from utils import get_org_config
+from utils import get_smtp_url
+from jobs import SendEmailJob
+
 
 class UtilTest(unittest.TestCase):
     def test_getSMTP_url(self):
         smtp_url = get_smtp_url('foo@163.com')
         self.assertEqual(smtp_url, 'smtp.163.com')
 
+
 class WebAPITests(unittest.TestCase):
-    def test_getUserInfo(self):
+    def test_0_send_email(self):
+        with patch("yagmail.SMTP") as mock_smtp:
+            SendEmailJob().send_notice_email()  # threading中raise的错误不会被catch
+
+    def test_1_getUserInfo(self):
         client = app.test_client()
         response = client.get('/api/getUserInfo?token=token0')
-        self.assertEqual(response.status_code, 200)
-    def test_submitUserInfo(self):
+        self.assertEqual(json.loads(response.data)['code'], 0)
+
+    def test_2_submitUserInfo(self):
         client = app.test_client()
-        response = client.post('/api/submitUserInfo', 
-            json={"token":"token0", "name":"new name"})
-        self.assertEqual(response.status_code, 200)
+        with patch("yagmail.SMTP") as mock_smtp:
+            response = client.post('/api/submitUserInfo',
+                                   json={"token": "token0", "name": "new name"})
+            self.assertEqual(json.loads(response.data)['code'], 0)
+
     def test_submitPictures(self):
         client = app.test_client()
         client.options('/api/uploadImage')
-        image_file  = open('config/pic.jpg', 'rb')
+        image_file = open('config/pic.jpg', 'rb')
         response = client.post('/api/uploadImage',
-            data={'template': image_file},
-            headers={'token': '1234'})
+                               data={'template': image_file},
+                               headers={'token': '1234'})
         res_json = json.loads(response.data.decode('ascii'))
         self.assertEqual(res_json['code'], 0)
         image_file.close()
 
     def test_addUser(self):
         client = app.test_client()
-        json_data = {"token":"1234", "email":["abc@example.org", "abc.org"]}
-        response = client.post('/api/addUserData',
-            json=json_data)
-        response = client.post('/api/addUserData',json=json_data)
+        json_data = {"token": "1234", "email": ["abc@example.org", "abc.org"]}
+        response = client.post('/api/addUserData', json=json_data)
         res_json = json.loads(response.data.decode('ascii'))
         self.assertEqual(res_json['code'], 0)
 
     def test_addUserRejected(self):
         client = app.test_client()
-        json_data = {"token":"5678", "email":"def@example.org"}
-        response = client.post('/api/addUserData',json=json_data)
+        json_data = {"token": "5678", "email": "def@example.org"}
+        response = client.post('/api/addUserData', json=json_data)
         res_json = json.loads(response.data.decode('ascii'))
         self.assertEqual(res_json['code'], 1)
 
     def test_updateOrgConfig(self):
         client = app.test_client()
         json_data = {"token": "1234",
-                     "website":"https://community.wuhan2020.org.cn/",
+                     "website": "https://community.wuhan2020.org.cn/",
                      "username": "admin@example.org",
                      "password": "pswd"}
         response = client.post('/api/updateOrgConfig',
-                                json=json_data,
-                                headers={'Referer': 'http://example.org/admin.html'})
+                               json=json_data,
+                               headers={'Referer': 'http://example.org/admin.html'})
         res_json = json.loads(response.data.decode('ascii'))
         self.assertEqual(res_json['code'], 0)
         email_config = get_email_config()
@@ -84,19 +86,6 @@ class WebAPITests(unittest.TestCase):
         res_json = json.loads(response.data.decode('ascii'))
         self.assertEqual(res_json['code'], 1)
 
-class SubmitUserInfoTests(unittest.TestCase):
-    def test_generate_image_and_send_email(self):
-        if not os.path.isdir("images"):
-            os.mkdir("images")
-        if not os.path.exists('pic.jpg'):
-            img = Image.new('RGB', (750, 1200))
-            img.save('pic.jpg')
-        with patch("yagmail.SMTP") as mock_smtp:
-            write_to_pic('test name', 'muxxs@foxmail.com', 'idle_token')
-
-    def test_notice_email(self):
-        with patch("yagmail.SMTP"):
-            SendEmailJob().start()
 
 class DbOperationTests(unittest.TestCase):
     def test_insert_people(self):
@@ -105,7 +94,8 @@ class DbOperationTests(unittest.TestCase):
             insert_people('abc@example.org', 'fake name')
         except:
             pass
- 
+
+
 if __name__ == '__main__':
     copyfile('config/data.json', 'data.json')
     unittest.main()
